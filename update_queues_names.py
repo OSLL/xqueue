04685710fa2b7eb8658eb7d1cad891a1d6139832
SuperSolik <serpetr99@gmail.com>
2020-07-25 20:07:26 +0300

    fixed regex bug

--- update_queues_names.py
+++ update_queues_names.py
@@ -8,12 +8,14 @@ def replace_dct_source(source, dct_name, new_dct_source):
     """Finds dct_name entry and replaces it with new source
     if dct_name entry is not found, then places new source at the end
     """
-    regex = re.compile("(" + dct_name + "\s*=\s*{(.*\n?)*})", re.MULTILINE)
+    regex = re.compile("(" + dct_name + "\s*=\s*\{([^\}]*\n?)*\})", re.MULTILINE)
     result = regex.search(source)
     if result is not None:
         entry_start, entry_end = result.span()
+        print('Found {} entry, replacing'.format(dct_name))
         source = source[:entry_start] + new_dct_source + source[entry_end:]
     else:
+        print('{} entry has not been found, adding at the end of file'.format(dct_name))
         source += '\n' + new_dct_source
 
     return source
@@ -30,7 +32,7 @@ if __name__ == '__main__':
         queues_names_dct = json_content.get('queue_names')
         if queues_names_dct is None:
             queues_names_dct = {}
-        new_xqueues = 'XQUEUES = {}'.format(json.dumps(queues_names_dct, sort_keys=True, indent=4))
+        new_xqueues = 'XQUEUES = {}\n'.format(json.dumps(queues_names_dct, sort_keys=True, indent=4))
 
     with open(args.module_name, 'r+') as f:
         source = f.read()
